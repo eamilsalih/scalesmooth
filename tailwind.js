6885b2bf9cec0e0922f8634e416560f4c93a4c34
eamilsalih <hopfer.lorant@gmail.com>
2025-10-17 16:44:57 +0200

    Add files via upload

---
+++ tailwind.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./**/*.html"
+    ],
+    theme: {
+      extend: {
+        // Add custom styles, colors or fonts here if needed
+      },
+    },
+    plugins: [],
+  }
+  
